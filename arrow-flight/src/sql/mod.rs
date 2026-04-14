7f255079ce8f9062e032a27c3498d34e75e0d8e1
w00476046 <wulei43@huawei.com>
2026-04-14 19:58:00 +0800

    b

---
+++ arrow-flight/src/sql/mod.rs
@@ -0,0 +1,377 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+//! Support for execute SQL queries using [Apache Arrow] [Flight SQL].
+//!
+//! [Flight SQL] is built on top of Arrow Flight RPC framework, by
+//! defining specific messages, encoded using the protobuf format,
+//! sent in the[`FlightDescriptor::cmd`] field to [`FlightService`]
+//! endpoints such as[`get_flight_info`] and [`do_get`].
+//!
+//! This module contains:
+//! 1. [prost] generated structs for FlightSQL messages such as [`CommandStatementQuery`]
+//! 2. Helpers for encoding and decoding FlightSQL messages: [`Any`] and [`Command`]
+//! 3. A [`FlightSqlServiceClient`] for interacting with FlightSQL servers.
+//! 4. A [`FlightSqlService`] to help building FlightSQL servers from [`FlightService`].
+//! 5. Helpers to build responses for FlightSQL metadata APIs: [`metadata`]
+//!
+//! [Flight SQL]: https://arrow.apache.org/docs/format/FlightSql.html
+//! [Apache Arrow]: https://arrow.apache.org
+//! [`FlightDescriptor::cmd`]: crate::FlightDescriptor::cmd
+//! [`FlightService`]: crate::flight_service_server::FlightService
+//! [`get_flight_info`]: crate::flight_service_server::FlightService::get_flight_info
+//! [`do_get`]: crate::flight_service_server::FlightService::do_get
+//! [`FlightSqlServiceClient`]: client::FlightSqlServiceClient
+//! [`FlightSqlService`]: server::FlightSqlService
+//! [`metadata`]: crate::sql::metadata
+use arrow_schema::ArrowError;
+use bytes::Bytes;
+use paste::paste;
+use prost::Message;
+
+#[allow(clippy::all)]
+mod r#gen {
+    // Since this file is auto-generated, we suppress all warnings
+    #![allow(missing_docs)]
+    include!("arrow.flight.protocol.sql.rs");
+}
+
+pub use r#gen::ActionBeginSavepointRequest;
+pub use r#gen::ActionBeginSavepointResult;
+pub use r#gen::ActionBeginTransactionRequest;
+pub use r#gen::ActionBeginTransactionResult;
+pub use r#gen::ActionCancelQueryRequest;
+pub use r#gen::ActionCancelQueryResult;
+pub use r#gen::ActionClosePreparedStatementRequest;
+pub use r#gen::ActionCreatePreparedStatementRequest;
+pub use r#gen::ActionCreatePreparedStatementResult;
+pub use r#gen::ActionCreatePreparedSubstraitPlanRequest;
+pub use r#gen::ActionEndSavepointRequest;
+pub use r#gen::ActionEndTransactionRequest;
+pub use r#gen::CommandGetCatalogs;
+pub use r#gen::CommandGetCrossReference;
+pub use r#gen::CommandGetDbSchemas;
+pub use r#gen::CommandGetExportedKeys;
+pub use r#gen::CommandGetImportedKeys;
+pub use r#gen::CommandGetPrimaryKeys;
+pub use r#gen::CommandGetSqlInfo;
+pub use r#gen::CommandGetTableTypes;
+pub use r#gen::CommandGetTables;
+pub use r#gen::CommandGetXdbcTypeInfo;
+pub use r#gen::CommandPreparedStatementQuery;
+pub use r#gen::CommandPreparedStatementUpdate;
+pub use r#gen::CommandStatementIngest;
+pub use r#gen::CommandStatementQuery;
+pub use r#gen::CommandStatementSubstraitPlan;
+pub use r#gen::CommandStatementUpdate;
+pub use r#gen::DoPutPreparedStatementResult;
+pub use r#gen::DoPutUpdateResult;
+pub use r#gen::Nullable;
+pub use r#gen::Searchable;
+pub use r#gen::SqlInfo;
+pub use r#gen::SqlNullOrdering;
+pub use r#gen::SqlOuterJoinsSupportLevel;
+pub use r#gen::SqlSupportedCaseSensitivity;
+pub use r#gen::SqlSupportedElementActions;
+pub use r#gen::SqlSupportedGroupBy;
+pub use r#gen::SqlSupportedPositionedCommands;
+pub use r#gen::SqlSupportedResultSetConcurrency;
+pub use r#gen::SqlSupportedResultSetType;
+pub use r#gen::SqlSupportedSubqueries;
+pub use r#gen::SqlSupportedTransaction;
+pub use r#gen::SqlSupportedTransactions;
+pub use r#gen::SqlSupportedUnions;
+pub use r#gen::SqlSupportsConvert;
+pub use r#gen::SqlTransactionIsolationLevel;
+pub use r#gen::SubstraitPlan;
+pub use r#gen::SupportedSqlGrammar;
+pub use r#gen::TicketStatementQuery;
+pub use r#gen::UpdateDeleteRules;
+pub use r#gen::XdbcDataType;
+pub use r#gen::XdbcDatetimeSubcode;
+pub use r#gen::action_end_transaction_request::EndTransaction;
+pub use r#gen::command_statement_ingest::TableDefinitionOptions;
+pub use r#gen::command_statement_ingest::table_definition_options::{
+    TableExistsOption, TableNotExistOption,
+};
+
+pub mod client;
+pub mod metadata;
+pub mod server;
+
+pub use crate::streams::FallibleRequestStream;
+
+/// ProstMessageExt are useful utility methods for prost::Message types
+pub trait ProstMessageExt: prost::Message + Default {
+    /// type_url for this Message
+    fn type_url() -> &'static str;
+
+    /// Convert this Message to [`Any`]
+    fn as_any(&self) -> Any;
+}
+
+/// Macro to coerce a token to an item, specifically
+/// to build the `Commands` enum.
+///
+/// See: <https://danielkeep.github.io/tlborm/book/blk-ast-coercion.html>
+macro_rules! as_item {
+    ($i:item) => {
+        $i
+    };
+}
+
+macro_rules! prost_message_ext {
+    ($($name:tt,)*) => {
+        paste! {
+            $(
+            const [<$name:snake:upper _TYPE_URL>]: &'static str = concat!("type.googleapis.com/arrow.flight.protocol.sql.", stringify!($name));
+            )*
+
+                as_item! {
+                /// Helper to convert to/from protobuf [`Any`] message
+                /// to a specific FlightSQL command message.
+                ///
+                /// # Example
+                /// ```rust
+                /// # use arrow_flight::sql::{Any, CommandStatementQuery, Command};
+                /// let flightsql_message = CommandStatementQuery {
+                ///   query: "SELECT * FROM foo".to_string(),
+                ///   transaction_id: None,
+                /// };
+                ///
+                /// // Given a packed FlightSQL Any message
+                /// let any_message = Any::pack(&flightsql_message).unwrap();
+                ///
+                /// // decode it to Command:
+                /// match Command::try_from(any_message).unwrap() {
+                ///   Command::CommandStatementQuery(decoded) => {
+                ///    assert_eq!(flightsql_message, decoded);
+                ///   }
+                ///   _ => panic!("Unexpected decoded message"),
+                /// }
+                /// ```
+                #[derive(Clone, Debug, PartialEq)]
+                pub enum Command {
+                    $(
+                        #[doc = concat!(stringify!($name), "variant")]
+                        $name($name),)*
+
+                    /// Any message that is not any FlightSQL command.
+                    Unknown(Any),
+                }
+            }
+
+            impl Command {
+                /// Convert the command to [`Any`].
+                pub fn into_any(self) -> Any {
+                    match self {
+                        $(
+                        Self::$name(cmd) => cmd.as_any(),
+                        )*
+                        Self::Unknown(any) => any,
+                    }
+                }
+
+                /// Get the URL for the command.
+                pub fn type_url(&self) -> &str {
+                    match self {
+                        $(
+                        Self::$name(_) => [<$name:snake:upper _TYPE_URL>],
+                        )*
+                        Self::Unknown(any) => any.type_url.as_str(),
+                    }
+                }
+            }
+
+            impl TryFrom<Any> for Command {
+                type Error = ArrowError;
+
+                fn try_from(any: Any) -> Result<Self, Self::Error> {
+                    match any.type_url.as_str() {
+                        $(
+                        [<$name:snake:upper _TYPE_URL>]
+                            => {
+                                let m: $name = Message::decode(&*any.value).map_err(|err| {
+                                    ArrowError::ParseError(format!("Unable to decode Any value: {err}"))
+                                })?;
+                                Ok(Self::$name(m))
+                            }
+                        )*
+                        _ => Ok(Self::Unknown(any)),
+                    }
+                }
+            }
+
+            $(
+                impl ProstMessageExt for $name {
+                    fn type_url() -> &'static str {
+                        [<$name:snake:upper _TYPE_URL>]
+                    }
+
+                    fn as_any(&self) -> Any {
+                        Any {
+                            type_url: <$name>::type_url().to_string(),
+                            value: self.encode_to_vec().into(),
+                        }
+                    }
+                }
+            )*
+        }
+    };
+}
+
+// Implement ProstMessageExt for all structs defined in FlightSql.proto
+prost_message_ext!(
+    ActionBeginSavepointRequest,
+    ActionBeginSavepointResult,
+    ActionBeginTransactionRequest,
+    ActionBeginTransactionResult,
+    ActionCancelQueryRequest,
+    ActionCancelQueryResult,
+    ActionClosePreparedStatementRequest,
+    ActionCreatePreparedStatementRequest,
+    ActionCreatePreparedStatementResult,
+    ActionCreatePreparedSubstraitPlanRequest,
+    ActionEndSavepointRequest,
+    ActionEndTransactionRequest,
+    CommandGetCatalogs,
+    CommandGetCrossReference,
+    CommandGetDbSchemas,
+    CommandGetExportedKeys,
+    CommandGetImportedKeys,
+    CommandGetPrimaryKeys,
+    CommandGetSqlInfo,
+    CommandGetTableTypes,
+    CommandGetTables,
+    CommandGetXdbcTypeInfo,
+    CommandPreparedStatementQuery,
+    CommandPreparedStatementUpdate,
+    CommandStatementIngest,
+    CommandStatementQuery,
+    CommandStatementSubstraitPlan,
+    CommandStatementUpdate,
+    DoPutPreparedStatementResult,
+    DoPutUpdateResult,
+    TicketStatementQuery,
+);
+
+/// An implementation of the protobuf [`Any`] message type
+///
+/// Encoded protobuf messages are not self-describing, nor contain any information
+/// on the schema of the encoded payload. Consequently to decode a protobuf a client
+/// must know the exact schema of the message.
+///
+/// This presents a problem for loosely typed APIs, where the exact message payloads
+/// are not enumerable, and therefore cannot be enumerated as variants in a [oneof].
+///
+/// One solution is [`Any`] where the encoded payload is paired with a `type_url`
+/// identifying the type of encoded message, and the resulting combination encoded.
+///
+/// Clients can then decode the outer [`Any`], inspect the `type_url` and if it is
+/// a type they recognise, proceed to decode the embedded message `value`
+///
+/// [`Any`]: https://developers.google.com/protocol-buffers/docs/proto3#any
+/// [oneof]: https://developers.google.com/protocol-buffers/docs/proto3#oneof
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Any {
+    /// A URL/resource name that uniquely identifies the type of the serialized
+    /// protocol buffer message. This string must contain at least
+    /// one "/" character. The last segment of the URL's path must represent
+    /// the fully qualified name of the type (as in
+    /// `path/google.protobuf.Duration`). The name should be in a canonical form
+    /// (e.g., leading "." is not accepted).
+    #[prost(string, tag = "1")]
+    pub type_url: String,
+    /// Must be a valid serialized protocol buffer of the above specified type.
+    #[prost(bytes = "bytes", tag = "2")]
+    pub value: Bytes,
+}
+
+impl Any {
+    /// Checks whether the message is of type `M`
+    pub fn is<M: ProstMessageExt>(&self) -> bool {
+        M::type_url() == self.type_url
+    }
+
+    /// Unpacks the contents of the message if it is of type `M`
+    pub fn unpack<M: ProstMessageExt>(&self) -> Result<Option<M>, ArrowError> {
+        if !self.is::<M>() {
+            return Ok(None);
+        }
+        let m = Message::decode(&*self.value)
+            .map_err(|err| ArrowError::ParseError(format!("Unable to decode Any value: {err}")))?;
+        Ok(Some(m))
+    }
+
+    /// Packs a message into an [`Any`] message
+    pub fn pack<M: ProstMessageExt>(message: &M) -> Result<Any, ArrowError> {
+        Ok(message.as_any())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_type_url() {
+        assert_eq!(
+            TicketStatementQuery::type_url(),
+            "type.googleapis.com/arrow.flight.protocol.sql.TicketStatementQuery"
+        );
+        assert_eq!(
+            CommandStatementQuery::type_url(),
+            "type.googleapis.com/arrow.flight.protocol.sql.CommandStatementQuery"
+        );
+    }
+
+    #[test]
+    fn test_prost_any_pack_unpack() {
+        let query = CommandStatementQuery {
+            query: "select 1".to_string(),
+            transaction_id: None,
+        };
+        let any = Any::pack(&query).unwrap();
+        assert!(any.is::<CommandStatementQuery>());
+        let unpack_query: CommandStatementQuery = any.unpack().unwrap().unwrap();
+        assert_eq!(query, unpack_query);
+    }
+
+    #[test]
+    fn test_command() {
+        let query = CommandStatementQuery {
+            query: "select 1".to_string(),
+            transaction_id: None,
+        };
+        let any = Any::pack(&query).unwrap();
+        let cmd: Command = any.try_into().unwrap();
+
+        assert!(matches!(cmd, Command::CommandStatementQuery(_)));
+        assert_eq!(cmd.type_url(), COMMAND_STATEMENT_QUERY_TYPE_URL);
+
+        // Unknown variant
+
+        let any = Any {
+            type_url: "fake_url".to_string(),
+            value: Default::default(),
+        };
+
+        let cmd: Command = any.try_into().unwrap();
+        assert!(matches!(cmd, Command::Unknown(_)));
+        assert_eq!(cmd.type_url(), "fake_url");
+    }
+}
---
+++ arrow-flight/src/utils.rs
@@ -0,0 +1,116 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+//! Utilities to assist with reading and writing Arrow data as Flight messages
+
+use crate::{FlightData, SchemaAsIpc};
+use std::collections::HashMap;
+use std::sync::Arc;
+
+use arrow_array::{ArrayRef, RecordBatch};
+use arrow_buffer::Buffer;
+use arrow_ipc::convert::fb_to_schema;
+use arrow_ipc::writer::CompressionContext;
+use arrow_ipc::{reader, root_as_message, writer, writer::IpcWriteOptions};
+use arrow_schema::{ArrowError, Schema, SchemaRef};
+
+/// Convert a slice of wire protocol `FlightData`s into a vector of `RecordBatch`es
+pub fn flight_data_to_batches(flight_data: &[FlightData]) -> Result<Vec<RecordBatch>, ArrowError> {
+    let schema = flight_data.first().ok_or_else(|| {
+        ArrowError::CastError("Need at least one FlightData for schema".to_string())
+    })?;
+    let message = root_as_message(&schema.data_header[..])
+        .map_err(|_| ArrowError::CastError("Cannot get root as message".to_string()))?;
+
+    let ipc_schema: arrow_ipc::Schema = message
+        .header_as_schema()
+        .ok_or_else(|| ArrowError::CastError("Cannot get header as Schema".to_string()))?;
+    let schema = fb_to_schema(ipc_schema);
+    let schema = Arc::new(schema);
+
+    let mut batches = vec![];
+    let dictionaries_by_id = HashMap::new();
+    for datum in flight_data[1..].iter() {
+        let batch = flight_data_to_arrow_batch(datum, schema.clone(), &dictionaries_by_id)?;
+        batches.push(batch);
+    }
+    Ok(batches)
+}
+
+/// Convert `FlightData` (with supplied schema and dictionaries) to an arrow `RecordBatch`.
+pub fn flight_data_to_arrow_batch(
+    data: &FlightData,
+    schema: SchemaRef,
+    dictionaries_by_id: &HashMap<i64, ArrayRef>,
+) -> Result<RecordBatch, ArrowError> {
+    // check that the data_header is a record batch message
+    let message = arrow_ipc::root_as_message(&data.data_header[..])
+        .map_err(|err| ArrowError::ParseError(format!("Unable to get root as message: {err:?}")))?;
+
+    message
+        .header_as_record_batch()
+        .ok_or_else(|| {
+            ArrowError::ParseError(
+                "Unable to convert flight data header to a record batch".to_string(),
+            )
+        })
+        .map(|batch| {
+            reader::read_record_batch(
+                &Buffer::from(data.data_body.as_ref()),
+                batch,
+                schema,
+                dictionaries_by_id,
+                None,
+                &message.version(),
+            )
+        })?
+}
+
+/// Convert `RecordBatch`es to wire protocol `FlightData`s
+pub fn batches_to_flight_data(
+    schema: &Schema,
+    batches: Vec<RecordBatch>,
+) -> Result<Vec<FlightData>, ArrowError> {
+    let options = IpcWriteOptions::default();
+    let schema_flight_data: FlightData = SchemaAsIpc::new(schema, &options).into();
+    let mut dictionaries = vec![];
+    let mut flight_data = vec![];
+
+    let data_gen = writer::IpcDataGenerator::default();
+    let mut dictionary_tracker = writer::DictionaryTracker::new(false);
+    let mut compression_context = CompressionContext::default();
+
+    for batch in batches.iter() {
+        let (encoded_dictionaries, encoded_batch) = data_gen.encode(
+            batch,
+            &mut dictionary_tracker,
+            &options,
+            &mut compression_context,
+        )?;
+
+        dictionaries.extend(encoded_dictionaries.into_iter().map(Into::into));
+        flight_data.push(encoded_batch.into());
+    }
+
+    let mut stream = Vec::with_capacity(1 + dictionaries.len() + flight_data.len());
+
+    stream.push(schema_flight_data);
+    stream.extend(dictionaries);
+    stream.extend(flight_data);
+    let flight_data = stream;
+    Ok(flight_data)
+}
